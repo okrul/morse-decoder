2a1c3d29dbba3e6dae7251a2b791465d66feb2db
Okrul <okrul@list.ru>
2021-08-24 01:15:40 +0300

    feat: implement morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let arr = expr.match(/(.{1,10})/g);
+    let decString = ''; 
+
+    for (i=0; i < arr.length; i++){
+        
+        if (arr[i] == '**********') {
+            decString += ' ';
+        }
+        else {
+
+            let pos = arr[i].indexOf(1);
+            
+            let substr = arr[i].slice(pos);
+            let arr2 = substr.match(/(.{1,2})/g);
+
+
+            for (j = 0; j < arr2.length; j++) {
+                if (arr2[j] == '10'){
+                    arr2[j] = '.';
+                }
+                else {
+                    arr2[j] = '-'; 
+                }
+            }
+
+            
+            decString += MORSE_TABLE[arr2.join('')]; 
+        }  
+    }
+
+    return decString;
 }
 
 module.exports = {
